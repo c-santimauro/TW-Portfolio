f20d868492bf07d29c84db72022646b09e17daac
c-santimauro <csantimauro@gmail.com>
2024-06-10 23:24:14 -0400

    fix config

---
+++ assets/js/f81fd78d.45f807dc.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[8791],{6844:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/TW-Portfolio/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":5,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
